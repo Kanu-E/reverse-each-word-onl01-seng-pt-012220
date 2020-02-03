54ee4197ec2ef2d898575224c479be9ca82e806c
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-03 05:09:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 def reverse_each_word(sentence)
  sentence_split = sentence.split(" ")
   new_sentence = []
    new_sentence = sentence_split.collect {|word| word.reverse }
    new_sentence.join(" ")
+=======
+def reverse_each_word(array)
+  array.reverse
+>>>>>>> f835fae906266fa6fe751db08c5a933f81eb2ac3
 end
